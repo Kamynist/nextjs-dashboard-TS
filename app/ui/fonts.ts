687b824c6de06798de288464aa307f2eb254ba15
Алексей <svirskii03@mail.ru>
2023-11-26 18:36:02 +0300

    fix font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,11 +1,11 @@
 import { Inter } from 'next/font/google';
-import { Manrope } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
 
-export const manrope = Manrope({
+export const lusitana  = Lusitana ({
   weight: '400',
   subsets: [ 'latin' ],
-  variable: '--font-manrope',
+  variable: '--font-lusitana',
  })
