a8c4b61779230e704f4c9b6709240639f02a1ebe
Алексей <svirskii03@mail.ru>
2023-11-26 18:27:07 +0300

    Add new files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Manrope } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+
+export const manrope = Manrope({
+  weight: '400',
+  subsets: [ 'latin' ],
+  variable: '--font-manrope',
+ })
