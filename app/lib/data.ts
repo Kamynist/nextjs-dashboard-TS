b32ef986371576fa22cf5b25abf4bbac36ca39cb
Алексей <svirskii03@mail.ru>
2023-12-17 19:28:33 +0300

    add not found and more

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,6 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
     // console.log('Fetching revenue data...');
     const data = await sql<Revenue>`SELECT * FROM revenue`;
     // console.log('Data fetch completed after 3 seconds.');
@@ -164,7 +163,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
